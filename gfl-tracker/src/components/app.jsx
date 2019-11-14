78ff7bf0bba7e411634e424dbed0a91f0459c647
Jonathan <kakashi169@gmail.com>
2019-11-14 03:36:47 +0000

    created a basic header and title as well as slightly modifying CSS style sheets and linking them to where they need to be linked

--- gfl-tracker/src/components/app.jsx
+++ gfl-tracker/src/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './layout/header';
 // import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render () {
     return (
       <div>
+        <Header />
         pug pug
       </div>
     )
